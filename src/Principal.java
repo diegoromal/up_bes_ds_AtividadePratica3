8514a630b8187823f5e2fcb9354a710e459c603c
diegoromal <diego@qosit.com.br>
2024-05-04 11:03:03 -0300

    Implementado classe Sistema

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,11 @@
-import models.Gerente;
+import views.Sistema;
 
 public class Principal {
 
     public static void main(String[] args) {
-
-        Gerente g1 = new Gerente(1234, "João", 180f, 25f, "Desenvolvimento", 5000f);
-        System.out.println(g1.toString());
-        g1.trabalhar();
-        g1.relatarProgresso();
         
+        Sistema.executar();
+    
     }
     
 }
